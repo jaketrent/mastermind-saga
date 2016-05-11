528f652d2828b40d7271b3f9ac966708e27a9cce
jaketrent <trent.jake@gmail.com>
2016-05-11 16:15:04 -0600

    api placeholders

--- server/mastermind/game.js
+++ server/mastermind/game.js
@@ -5,12 +5,23 @@ const app = koa()
 
 app.use(route.post('/', create))
 app.use(route.get('/', list))
+app.use(route.get('/:id', show))
+app.use(route.del('/:id', del))
 
 function* create() {
   this.body = [{ created: 'game' }]
 }
+
 function* list() {
   this.body = [{ all: 'games' }]
+}
+
+function* show(id) {
+  this.body = { game: id }
+}
+
+function* del(id) {
+  console.log('deleted.')
 }
 
 module.exports = app
