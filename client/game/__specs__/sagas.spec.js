3b2d0877a5926ab945914120e2d3f552f3be460d
jaketrent <trent.jake@gmail.com>
2016-07-12 21:45:08 -0600

    tweak test naming, ensure generator done

--- client/game/__specs__/sagas.spec.js
+++ client/game/__specs__/sagas.spec.js
@@ -10,26 +10,29 @@ const { deserializeError, deserializeSuccess, formatUrl, request } = api.create
 test('#create handles a request success', t => {
   const res = { status: 200, data: { data: { some: 'response' } } }
 
-  const iterator = subject.create()
+  const gen = subject.create()
 
-  t.deepEqual(iterator.next().value, call(request, formatUrl()))
-  t.deepEqual(iterator.next(res).value, put(actions.createSuccess(deserializeSuccess(res))))
+  t.deepEqual(gen.next().value, call(request, formatUrl()))
+  t.deepEqual(gen.next(res).value, put(actions.createSuccess(deserializeSuccess(res))))
+  t.truthy(gen.next().done)
 })
 
 test('#create handles a request error', t => {
   const res = { status: 500, data: { errors: [{ some: 'error' }] } }
 
-  const iterator = subject.create()
+  const gen = subject.create()
 
-  t.deepEqual(iterator.next().value, call(request, formatUrl()))
-  t.deepEqual(iterator.throw(res).value, put(actions.createError(deserializeError(res))))
+  t.deepEqual(gen.next().value, call(request, formatUrl()))
+  t.deepEqual(gen.throw(res).value, put(actions.createError(deserializeError(res))))
+  t.truthy(gen.next().done)
 })
 
 test('#create handles thrown error', t => {
   const err = new Error('some error')
 
-  const iterator = subject.create()
+  const gen = subject.create()
 
-  t.deepEqual(iterator.next().value, call(request, formatUrl()))
-  t.throws(_ => iterator.next())
+  t.deepEqual(gen.next().value, call(request, formatUrl()))
+  t.throws(_ => gen.next())
+  t.truthy(gen.next().done)
 })
--- client/guess/__specs__/sagas.spec.js
+++ client/guess/__specs__/sagas.spec.js
@@ -12,26 +12,29 @@ const { deserializeError, deserializeSuccess, formatUrl, request, serialize } = 
 test('#create handles request success', t => {
   const res = { status: 201, data: { data: [{ some: 'response' }] } }
 
-  const iterator = subject.create({ id, guess })
+  const gen = subject.create({ id, guess })
 
-  t.deepEqual(iterator.next().value, call(request, formatUrl(id), serialize(guess)))
-  t.deepEqual(iterator.next(res).value, put(actions.createSuccess(guess, deserializeSuccess(res))))
+  t.deepEqual(gen.next().value, call(request, formatUrl(id), serialize(guess)))
+  t.deepEqual(gen.next(res).value, put(actions.createSuccess(guess, deserializeSuccess(res))))
+  t.truthy(gen.next().done)
 })
 
 test('#create handles request error', t => {
   const res = { status: 400, data: { errors: [{ id: 'anId', title: 'response' }] } }
 
-  const iterator = subject.create({ id, guess })
+  const gen = subject.create({ id, guess })
 
-  t.deepEqual(iterator.next().value, call(request, formatUrl(id), serialize(guess)))
-  t.deepEqual(iterator.throw(res).value, put(actions.createError(deserializeError(res))))
+  t.deepEqual(gen.next().value, call(request, formatUrl(id), serialize(guess)))
+  t.deepEqual(gen.throw(res).value, put(actions.createError(deserializeError(res))))
+  t.truthy(gen.next().done)
 })
 
 test('#create handles thrown error', t => {
   const err = new Error('some error')
 
-  const iterator = subject.create({ id, guess })
+  const gen = subject.create({ id, guess })
 
-  t.deepEqual(iterator.next().value, call(request, formatUrl(id), serialize(guess)))
-  t.throws(_ => iterator.throw(err))
+  t.deepEqual(gen.next().value, call(request, formatUrl(id), serialize(guess)))
+  t.throws(_ => gen.throw(err))
+  t.truthy(gen.next().done)
 })
